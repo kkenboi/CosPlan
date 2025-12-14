e0145a95288715fdbe7ca40a1fe1be3a55f44639
Kenji <116390511+kkenboi@users.noreply.github.com>
2025-12-15 06:12:26 +0800

    .env and testing sql queries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,30 @@
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:8081/')
+    fetch('http://localhost:8081/users')
       .then(res => res.json())
       .then(data => setData(data))
       .catch(err => console.log(err));
   }, [])
-
+  
   return (
       <div style={{ padding: "50px" }}>
         <h1>Frontend-Backend Connection</h1>
         {/* If data exists, show it. Otherwise show "Loading..." */}
-        <h3>Result from API: {data ? data : "Loading..."}</h3>
+        <h3>Result from API: {data.length ? data.length : "Loading..." }</h3>
+            {data.map((user, index) => (
+              <tr key={index}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+              </tr>
+            ))}
       </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
+// express application instance
 const app = express();
 
 // enable cors to talk to react
@@ -9,14 +12,24 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'anihideout'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
+// here is where we are doing our GET and POST route definitions
+// -> most likely we can move this to another page soon, shouldn't be here
 app.get('/', (req, res) => {
     return res.json("From Backend Side");
+});
+
+app.get('/users', (req, res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    });
 });
 
 app.listen(8081, () => {
