6d460cb27b90302471b3297ccf200e90f97efb7c
Kenji <116390511+kkenboi@users.noreply.github.com>
2025-12-15 16:06:30 +0800

    concurrency library addition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,18 +14,18 @@ function App() {
   }, [])
   
   return (
-      <div style={{ padding: "50px" }}>
-        <h1>Frontend-Backend Connection</h1>
-        {/* If data exists, show it. Otherwise show "Loading..." */}
-        <h3>Result from API: {data.length ? data.length : "Loading..." }</h3>
-            {data.map((user, index) => (
-              <tr key={index}>
-                  <td>{user.id}</td>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-              </tr>
-            ))}
-      </div>
+    <div style={{ padding: "50px" }}>
+      <h1>Frontend-Backend Connection</h1>
+      {/* If data exists, show it. Otherwise show "Loading..." */}
+      <h3>Result from API: {data.length ? data.length : "Loading..." }</h3>
+        {data.map((user, index) => (
+          <tr key={index}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+          </tr>
+        ))}
+    </div>
     )
 }
 
